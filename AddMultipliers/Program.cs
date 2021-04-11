e5228a37da555d71d261262f382d9aa3b5f42a94
Kagarevich <68788312+Kagarevich@users.noreply.github.com>
2021-04-11 15:25:19 +0300

    FirstTest

---
+++ AddMultipliers/Program.cs
@@ -0,0 +1,28 @@
+﻿using StackExchange.Redis;
+
+namespace RedisConnecion
+{
+    class Program
+    {
+        static void Main()
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            //Таблица по умолчанию
+            var db = redis.GetDatabase();
+            
+            
+            //ключи для двух значений
+            const string keyFirst = "testFirstKey";
+            const string keySecond = "testSecondKey";
+            
+            //Ввод значений множителей и их последующая записаь в бд
+            System.Console.WriteLine("Enter the first multiplier:");
+            db.StringSet(keyFirst, System.Convert.ToDouble(System.Console.ReadLine()));
+            System.Console.WriteLine("Enter the second multiplier:");
+            db.StringSet(keySecond, System.Convert.ToDouble(System.Console.ReadLine()));
+        }
+    }
+}
---
+++ Multiplication/Program.cs
@@ -0,0 +1,34 @@
+﻿using StackExchange.Redis;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost", 6379);
+
+            var redis = ConnectionMultiplexer.Connect(options);
+            var db = redis.GetDatabase();
+
+            const string keyFrist = "testFirstKey";
+            const string keySecond = "testSecondKey";
+
+            var firstMultiplier = System.Convert.ToDouble(db.StringGet(keyFrist));
+            var secondMultiplier = System.Convert.ToDouble(db.StringGet(keySecond));
+            
+            
+            System.Console.WriteLine("Press F to get multiplication");
+            if (System.Console.ReadKey().KeyChar == 70)
+            {
+                GetMultiplication(firstMultiplier, secondMultiplier);
+            }
+        }
+
+        static void GetMultiplication(double firstMultiplier, double secondMultiplier)
+        {
+            System.Console.WriteLine($"\n{firstMultiplier} * {secondMultiplier} = {firstMultiplier * secondMultiplier}");
+        }
+    }
+}
